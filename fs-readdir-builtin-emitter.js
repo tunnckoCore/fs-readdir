4a2e0c3ec24617299650ed85615ca0ed1520ec2c
tunnckoCore <mameto_100@mail.bg>
2015-02-05 16:59:42 +0200

    bench only stream and callback api

--- fs-readdir-builtin-emitter.js
+++ fs-readdir-builtin-emitter.js
@@ -1,6 +1,6 @@
 var self = require('./index');
 var EventEmitter = require('events').EventEmitter;
 
-module.exports = function index(root, cb) {
-  return self(root, {EventEmitter: EventEmitter}, cb).on('finish', cb);
-}
+// module.exports = function index(root, cb) {
+//   return self(root, {EventEmitter: EventEmitter}, cb).on('finish', cb);
+// };
--- fs-readdir-dual-emitter.js
+++ fs-readdir-dual-emitter.js
@@ -1,6 +1,6 @@
 var self = require('./index');
 var DualEmitter = require('dual-emitter');
 
-module.exports = function index(root, cb) {
-  return self(root, {EventEmitter: DualEmitter}, cb).on('finish', cb);
-}
+// module.exports = function index(root, cb) {
+//   return self(root, {EventEmitter: DualEmitter}, cb).on('finish', cb);
+// };
